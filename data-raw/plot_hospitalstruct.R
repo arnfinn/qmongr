adefe5cf30e698614966296b93ee7ec79f12c1e9
arnfinn <arnfinn.steindal@gmail.com>
2020-02-12 16:06:24 +0100

    Better structure and less repeat

--- data-raw/plot_hospitalstruct.R
+++ data-raw/plot_hospitalstruct.R
@@ -1,8 +1,9 @@
 
-require(visNetwork)
-
 nodes <- read.csv("data-raw/nodes.csv", colClasses = "character", sep = ";")
 edges <- read.csv("data-raw/edges.csv", colClasses = "character", sep = ";")
 
-visNetwork(nodes, edges)
-
+visNetwork::visNetwork(nodes, edges) %>% 
+  visNetwork::visGroups(groupname = "RHF", color = "#deebf7", shape = "box") %>%
+  visNetwork::visGroups(groupname = "HF", color = "#9ecae1", shape = "box") %>%
+  visNetwork::visGroups(groupname = "Sh", color = "#3182bd", shape = "box") %>% 
+  visNetwork::visHierarchicalLayout(direction = "LR", sortMethod = "directed", levelSeparation = 300) 
